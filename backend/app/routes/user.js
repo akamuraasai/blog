4845ee5cc428c9d730cdbcf14817753a392950c4
Jonathan Willian <jonathan.willian.tod@gmail.com>
2021-08-22 23:09:02 +0000

    Created the route code for the user model.

--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -2,14 +2,76 @@ const { User } = require('../models');
 
 module.exports = (app) => {
   app.post('/register', async (req, res) => {
-    const { body } = req;
-    const user = await User.create(body);
-    res.json(user);
+    const { name, email, password } = req.body;
+
+    if (!name || typeof name !== 'string' || name.length == 0) {
+      return res.status(400).send('field "name" was not present');
+    }
+
+    if (!email || typeof email !== 'string' || email.length == 0) {
+      return res.status(400).send('field "email" was not present');
+    }
+
+    if (!password || typeof password !== 'string' || password.length == 0) {
+      return res.status(400).send('field "password" was not present');
+    }
+
+    const user = await User.create({ name, email, password });
+    return res.json(user);
   });
 
-  app.get('/users', (req, res) => {}); //Listar todos
-  app.post('/users', (req, res) => {}); // Criar
-  app.get('/users/:id', (req, res) => {}); //Buscar
-  app.put('/users/:id', (req, res) => {}); //Editar
-  app.delete('/users/:id', (req, res) => {}); //Deletar
+  app.get('/users', async (req, res) => {
+    const users = await User.findAll();
+    return res.send(users);
+  });
+
+  app.get('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    if (!id || id === '') {
+      return res.status(404).send('not found');
+    }
+    const user = await User.findOne({ where: { id } });
+    return res.send(user);
+  });
+
+  app.put('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    if (!id || id === '') {
+      return res.status(404).send('not found');
+    }
+
+    const user = await User.findOne({ where: { id } });
+    if (!user) {
+      return res.status(404).send('not found');
+    }
+
+    if (name && typeof name === 'string' && name.length > 0) {
+      user.name = name;
+    }
+
+    if (email && typeof email === 'string' && email.length > 0) {
+      user.email = email;
+    }
+
+    await user.save();
+    return res.send(user);
+  });
+
+  app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+
+    if (!id || id === '') {
+      return res.status(404).send('not found');
+    }
+
+    const user = await User.findOne({ where: { id } });
+    if (!user) {
+      return res.status(404).send('not found');
+    }
+
+    await user.destroy();
+    return res.send(user);
+  });
 };
