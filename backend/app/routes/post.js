bd4ac0ea5ca269b2ca084b4674c78f0ece874e5b
RobsonStrife <robson.op_s@hotmail.com>
2021-08-22 22:14:37 -0300

    Complete Routes Post

--- backend/app/routes/post.js
+++ backend/app/routes/post.js
@@ -1,21 +1,87 @@
 const { Post } = require('../models');
 
-  app.get('/posts', (req, res) => {
-      Post.findAll();
+module.exports = (app) => {
+  app.get('/posts', async (req, res) => {
+      const posts = await Post.findAll();
+      return res.send(posts);
   }); //Listar todos
 
-  app.post('/posts', async (req, res) => {
-      const {body} = req;
-      const post = await  Post.create();
-      res.Json(Post);
-  }); // Criar
+  app.get('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    if (!id || id === '') {
+      return res.status(404).send('not found');
+    }
 
-  app.get('/posts/:id', (req, res) => {
-    const {body} = req;
-    const post = await  Post.findAt();
-    res.Json(Post);
+    const post = await Post.findOne({ where: { id } });
+    return res.send(post);
   }); //Buscar
 
-  app.put('/posts/:id', (req, res) => {}); //Editar
+  app.post('/posts', async (req, res) => {
+      const { user_id: userId, post: body, title, banner } = req.body;
 
-  app.delete('/posts/:id', (req, res) => {}); //Deletar
+      if (!userId || typeof userId !== 'number' || userId === 0) {
+        return res.status(400).send('field "user_id" was not present');
+      }
+
+      if (!body || typeof body !== 'string' || body.length === 0) {
+        return res.status(400).send('field "post" was not present');
+      }
+
+      if (!title || typeof title !== 'string' || title.length === 0) {
+        return res.status(400).send('field "title" was not present');
+      }
+
+      const post = await Post.create({ user_id: userId, post: body, title, banner });
+      return res.json(post);
+
+  }); // Criar
+
+  app.put('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    const {  user_id: userId, post: body, title, banner } = req.body;
+
+    if (!id || id === '' || id === 0) {
+        return res.status(404).send('not found');
+    }
+    const post = await Post.findOne({ where: { id } });
+
+    if (!post) {
+        return res.status(404).send('not found');
+    }
+
+    if (userId || typeof userId === 'number' || userId > 0) {
+        post.user_id = userId;
+    }
+
+    if (post || typeof post === 'string' || post.length > 0) {
+        post.post = body;
+    }
+
+    if (title || typeof title === 'string' || title.length > 0) {
+        post.title = title;
+    }
+
+    if(banner || typeof banner === 'string' || banner.length > 0 ){
+        post.banner = banner;
+    }
+
+    await post.save();
+    return res.send(post);
+  }); //Editar
+
+  app.delete('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+
+    if (!id || id === '' || id === 0) {
+        return res.status(404).send('not found');
+    }
+    const post = await Post.findOne({ where: { id } });
+
+    if (!post) {
+        return res.status(404).send('not found');
+    }
+
+    await post.destroy();
+    return res.send(post);
+  }); //Deletar
+};
